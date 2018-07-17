c0f112bd17064ba617eb55842033a03e75cd6dad
DevClass <support-2@ahsmail.com>
2018-07-17 11:49:17 -0600

    built our first functional component

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div classname="Header">
+        <h1>Bottega Madman</h1>
+        <p>Just great, right?</p>
+        </div>
+    )
+}
+
+
+export default Header;
